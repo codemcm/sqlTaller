53e54e16b6d9a9cea30fed2f37aa40e2fc7201e2
Julio César López Hernández <jc_lopezhernandez@outlook.com>
2020-11-22 15:50:36 -0600

    Tabla organizer

---
+++ consultasJulio.sql
@@ -0,0 +1,30 @@
+INSERT INTO organizer(name, created_by)
+    VALUES ('Instituto Tecnologico', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Universidad Tecnologica', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Preparatoria Federal', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('MIT', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('UGM', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Universidad Popular',1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('BUAP', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('UVP', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('UNAM', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Universidad de Tecamachalco', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('UAG', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Instituto Nacional de Enfermeria', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Instituto Plitecnico Nacional', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Universidad de Oaxaca', 1);
+INSERT INTO organizer(name, created_by)
+    VALUES ('Normal Superior', 1);
